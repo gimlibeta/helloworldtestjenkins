4ee8e82aed2bc96d2e53731ad0fd0cd17e5536d0
gimlibeta <gimlibeta@orange.fr>
2017-03-03 16:43:21 +0100

    Update TestConstantes.java

--- webapp-maven/src/test/java/sample/TestConstantes.java
+++ webapp-maven/src/test/java/sample/TestConstantes.java
@@ -7,8 +7,5 @@ public class TestConstantes {
     public void testHelloWorld(){
         Assert.assertEquals("YDOB-ON", Constantes.WHO);
         System.out.println("Hello "+Constantes.WHO);
-
-	Assert.assertEquals("NO-BODY", Constantes2.WHO2);
-        System.out.println("Hello "+Constantes2.WHO2);
     }
 }

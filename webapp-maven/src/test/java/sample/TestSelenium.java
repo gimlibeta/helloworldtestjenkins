7cb79f32ef22c4c07463d2bfaa34ad2b2e87c9fc
gimlibeta <gimlibeta@orange.fr>
2017-03-03 16:37:45 +0100

    Comment imports

--- webapp-maven/src/test/java/sample/TestSelenium.java
+++ webapp-maven/src/test/java/sample/TestSelenium.java
@@ -1,10 +1,10 @@
 package sample;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
+/*import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;*/
 
 public class TestSelenium {
    	@Test

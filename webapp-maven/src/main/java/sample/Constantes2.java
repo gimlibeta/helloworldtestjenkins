b476ebbf7a487555e689d6709214ac6c64edc377
gimlibeta <gimlibeta@orange.fr>
2017-03-03 16:41:22 +0100

    Update Constantes2.java

--- webapp-maven/src/main/java/sample/Constantes2.java
+++ webapp-maven/src/main/java/sample/Constantes2.java
@@ -2,7 +2,7 @@ package sample;
 
 public class Constantes2 {
 
-	public static final String WHO = System.getProperty("hello.who", "NO-BODY");
+	public static final String WHO = System.getProperty("hello.who2", "NO-BODY");
 }
 
 
